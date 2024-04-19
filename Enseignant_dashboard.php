7b8dcefebaad43420793c97a7043760c275f0ecf
9ada19 <98662869+9ada19@users.noreply.github.com>
2024-04-19 16:43:01 +0000

    Update Enseignant_dashboard.php

--- Enseignant_dashboard.php
+++ Enseignant_dashboard.php
@@ -11,14 +11,35 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <h2>Tableau de Bord Enseignant</h2>
-        <div class="cours">
-            <h2>Vos Cours</h2>
-            <div class="exercices">Ajouter des exercices</div>
-            
-    </div>
+    <div class="page">
+    <!-- Header -->
+        <div class="header">
+            <div class="image_header">
+                <img class="img_univ" src="img/ump2.png">
+            </div>
+            <div class="title_header">
+                <h1>EduFSO</h1>
+            </div>
+        </div>
         
+        <div class="divider"></div>
+    <!-- body -->
+        <div class="bodd">
+            <div class="container">
+                <h2>Tableau de Bord Enseignant</h2>
+                <div class="cours">
+                    <h2>Vos Cours</h2>
+                    <div class="exercices">Ajouter des exercices</div>
+            </div>
+            
+        </div>
+    <!-- Footer -->
+        <div class="footer">
+
+        </div>
+
+            
+        </div>
     </div>
 </body>
 </html>
